ce065c5c82311c3f964c54e6e328844d0c384fb9
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-07-05 22:15:57 -0400

    pictured rendered using template literals for targetting, WIN

--- src/components/survivors/Survivor.js
+++ src/components/survivors/Survivor.js
@@ -7,11 +7,19 @@ const Survivor = props => {
   return (
     <div className='survivor-card'>
       <h2>{props.name}</h2>
-      <img src={`../../assets/Survivors/${props.name}.png`} alt={props.name} />
+      <img
+        src={require(`../../assets/Survivors/${props.name}.png`)}
+        alt={props.name}
+        className='survivor-img'
+      />
       {props.perks.map(perk => {
         return (
           <div className='perks'>
-            <img src={`../../assets/Perks/${perk}.gif`} alt={perk} />
+            <img
+              src={require(`../../assets/Perks/Survivor-Perks/${perk}.gif`)}
+              alt={perk}
+              className='survivor-perk'
+            />
             <p>{perk}</p>
           </div>
         );
--- src/components/survivors/SurvivorsList.js
+++ src/components/survivors/SurvivorsList.js
@@ -9,19 +9,21 @@ export default function SurvivorsList(props) {
     <div className='page'>
       <div>
         <div className='section-title'>
-          <h1>Survivors</h1>
+          <h1>All Survivors</h1>
         </div>
-        {props.characters.map(surv => {
-          return (
-            <Survivor
-              name={surv.name}
-              bio={surv.bio}
-              perks={surv.perks}
-              difficulty={surv.difficulty}
-              key={surv.id}
-            />
-          );
-        })}
+        <div className='survivors'>
+          {props.characters.map(surv => {
+            return (
+              <Survivor
+                name={surv.name}
+                bio={surv.bio}
+                perks={surv.perks}
+                difficulty={surv.difficulty}
+                key={surv.id}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
